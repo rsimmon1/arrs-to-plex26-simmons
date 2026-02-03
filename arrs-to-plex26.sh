9ed899d9e687bc7270304b33e51b4bc3f2bf7aef
rsimmon1 <168495240+rsimmon1@users.noreply.github.com>
2026-02-03 09:16:08 -0800

    first edits to change static values

--- arrs-to-plex26.sh
+++ arrs-to-plex26.sh
@@ -3,28 +3,24 @@
 set -e
 
 # Define arrays for mount points and network shares
-QNAP_SHARES="//plexd.randrservices.com/PlexData"
-
-QNAP_MOUNTS="/mnt/qnap"
-
-QNAP_FOLDERS=(
+PLEX26_SHARES="//plex26.randrservices.com/PlexData"
+PLEX26_MOUNTS="/mnt/plex26"
+PLEX26_FOLDERS=(
     "Movies"
     "TV Shows"
-    "Books"
-    "iTunes/iTunes Media"
 )
 
 ARRS_LOCATION="/srv/media/"
-
 ARRS_FOLDERS=(
     "movies"
     "tvshows"
-    "books"
-    "music"
 )
 
 # Define Trigger file name if there was a file/folder to copy
 TRIGGER_FILE="TRIGGERCOPY.TXT"
+
+# Path to store the last run timestamp
+LOG_FILE="log_run.txt"
 
 # Function to check and mount if not already mounted using findmnt
 mount_if_needed() {
@@ -51,12 +47,11 @@ mount_if_needed() {
 }
 
 # Mount the shares to the specified mount points
-mount_if_needed "$QNAP_SHARES" "$QNAP_MOUNTS"
+mount_if_needed "$PLEX26_SHARES" "$PLEX26_MOUNTS"
 
 df -h
 
-# Path to store the last run timestamp
-LOG_FILE="log_run.txt"
+
 
 while true; do
     # Current time in seconds since the epoch
@@ -72,16 +67,16 @@ while true; do
         if [ -n "$(ls -A $ARRS_LOCATION${ARRS_FOLDERS[i]} 2>/dev/null)" ]
         then
             echo
-            echo "*************** $ARRS_LOCATION${ARRS_FOLDERS[i]} to $QNAP_MOUNTS/${QNAP_FOLDERS[i]} ***************"
+            echo "*************** $ARRS_LOCATION${ARRS_FOLDERS[i]} to $PLEX26_MOUNTS/${PLEX26_FOLDERS[i]} ***************"
             # show the files and folders we will copy
             ls "$ARRS_LOCATION${ARRS_FOLDERS[i]}" || true
             # rsync the files and folders
-            rsync -r -ah --remove-source-files -P "$ARRS_LOCATION${ARRS_FOLDERS[i]}"/ "$QNAP_MOUNTS/${QNAP_FOLDERS[i]}" || true
+            rsync -r -ah --remove-source-files -P "$ARRS_LOCATION${ARRS_FOLDERS[i]}"/ "$PLEX26_MOUNTS/${PLEX26_FOLDERS[i]}" || true
             # erase the folders and files if left over
             find "$ARRS_LOCATION${ARRS_FOLDERS[i]}" -mindepth 1 -type d -empty -delete || true
             # create trigger file to say we did a copy
-            echo "${START_TIME}">"$QNAP_MOUNTS/${QNAP_FOLDERS[i]}/$TRIGGER_FILE"
-            echo "*************** $ARRS_LOCATION${ARRS_FOLDERS[i]} to $QNAP_MOUNTS/${QNAP_FOLDERS[i]} Done ***************"
+            echo "${START_TIME}">"$PLEX26_MOUNTS/${PLEX26_FOLDERS[i]}/$TRIGGER_FILE"
+            echo "*************** $ARRS_LOCATION${ARRS_FOLDERS[i]} to $PLEX26_MOUNTS/${PLEX26_FOLDERS[i]} Done ***************"
         else
             echo -n " No files $ARRS_LOCATION${ARRS_FOLDERS[i]} "
         fi
